d2f7a0a40c7e799fad351819ca3c4c5f3223b313
Numan506 <78308167+Numan506@users.noreply.github.com>
2021-05-19 17:41:41 +0600

    Add files via upload

---
+++ Local module/Numan.js
@@ -0,0 +1,21 @@
+const getName =()=>{
+    return "Abu jakaria md Numan";
+}
+
+const getAge = ()=>{
+    return "24";
+}
+
+const CGPA = ()=>{
+    return "3.10";
+}
+
+exports.Name = getName;
+exports.Age = getAge;
+exports.result = CGPA;
+
+// module.exports = {
+//     getName,
+//     getAge,
+//     CGPA
+// }
---
+++ Local module/index.js
@@ -0,0 +1,4 @@
+const s1 = require("./Numan");
+console.log("Name is: "+s1.Name() );
+console.log("Age is: "+s1.Age() );
+console.log("Result is: "+s1.result());
